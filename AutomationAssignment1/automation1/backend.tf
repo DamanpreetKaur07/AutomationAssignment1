439b485af56b65bdf0ff840dc9d56d440e0676dd
Damanpreet Kaur <kaurdamanpreet207@gmail.com>
2024-03-13 03:42:27 -0400

    Assignment1

--- AutomationAssignment1/automation1/backend.tf
+++ AutomationAssignment1/automation1/backend.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "tfstaten01522692sa"
     container_name       = "tfstatefiles"
     key                  = "terraform.tfstate"
-    access_key           = ""
+  
   }
 }
